b1ff2220a4325c195cdeadfb7246a30b75695499
chris.capps@live.com <chris@lamp-post-mac.lan>
2023-01-05 17:09:49 -0500

    Fleshed out the skeleton

---
+++ tool/src/main/java/com/replay/tool/ReplayCli.java
@@ -0,0 +1,10 @@
+package com.replay.tool;
+
+import com.replay.io.Console;
+
+public class ReplayCli {
+    public static void main(String[] args) {
+        Console.initialize();
+        Console.replay("replay: 1.0");
+    }
+}
--- tool/src/main/java/replay/tool/ReplayCli.java
+++
@@ -1,9 +0,0 @@
-package replay.tool;
-
-import com.replay.io.Console;
-
-public class ReplayCli {
-    public static void main(String[] args) {
-        Console.replay("replay: 1.0");
-    }
-}
